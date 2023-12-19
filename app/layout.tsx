02721a1fd859362f41e04c875916840df56563e7
Phil Goo Kang <philgookang@gmail.com>
2023-12-19 11:11:04 +0900

    Feat: added vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
+            <SpeedInsights/>
         </html>
     )
 }
